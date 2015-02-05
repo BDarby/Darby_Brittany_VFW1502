0fb08d540895071c440527956b64a37c28ef7b39
BDarby <b_nicole@Brittanys-MacBook-Pro.local>
2015-02-05 16:17:43 -0500

    Darby_Brittany_Project1
    this is my project one

--- lectures/2.2 windows/Resources/app.js
+++ lectures/2.2 windows/Resources/app.js
@@ -18,7 +18,7 @@ win.open();
 
 
 
-var win=Titanium.UI.createWindow({
+/*var win=Titanium.UI.createWindow({
 	backgroundColor:"#6ACDBD",
 });
 var view=Titanium.UI.createView({
@@ -32,19 +32,60 @@ var title = Ti.UI.createLabel({
 });
 
 
-var shark=Ti.UI.createView({
+var view2=Ti.UI.createView({    // can't get this to work
 	backgroundImage:"sharks.jpeg",
-	width:200,
-	length:200,
+	width:500,
+	height:200,
 });
 
 
 
-win.add(shark);
+win.add(view2);
 win.add(title);
 win.add(view);  //this is like the console.log
 win.open();     //this is like the console.log
+*/
 
+//________________________________________________________________________________________________________________________
+var title= Titanium.UI.createLabel({
+	 text: "Pittsburgh Steelers", //this is the object
+	 color:"White",
+	 top:200
+	}); 
+
+
+
+var win=Titanium.UI.createWindow({
+	backgroundImage:"steelers.jpeg",  //this is background
+	width:500,
+	height:800,
+});
+
+
+var Steelers=["T.Polamalu", "Big Ben", "J.Harrison", "A,Brown", "M.Pouncey"];   //this is my array.
+
+
+var buttbar = Titanium.UI.createButtonBar({   //Titanium.UI.createButtonBar is the a function.
+    labels:["Previous", "Next"],//
+    backgroundColor:"#D8C9ED", //
+    top:100,                   //  this whole thing is the method
+    height:25,                 //
+    width:200,                 //
+});
+
+
+
+var animation = Titanium.UI.createAnimation();  //animation is the property
+animation.color = "red";
+animation.duration = 5000;
+title.animate(animation);
+
+
+
+win.add(buttbar);
+win.add(title);
+win.add(win);
+win.open();
 
 
 
